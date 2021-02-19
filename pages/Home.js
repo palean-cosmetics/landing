7b4d5eea05db1b702c60c19f02bbfabbe2c400a0
Andras Toth <andrastoth@Andrass-MacBook-Pro.local>
2021-02-19 19:44:27 +0100

    Initial version

---
+++ pages/Home.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './Home.module.css'
+import Logo from './logo.svg'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Palean</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Image loading="eager" className={styles.image} src="/bg.png" alt="Picture of the author" layout="fill" objectFit="cover" />
+
+      <div className={styles.container}>
+        <Logo className={styles.logo} />
+      </div>
+    </>
+  )
+}
